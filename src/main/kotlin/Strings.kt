de33e69afa0f9dd9ba922a299b5a3161ba6b2c7e
ZakyFAR <zackyfathinabdurrazaq36@gmail.com>
2020-08-06 19:02:32 +0700

    Terdapat Perubahan

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -1,9 +1,13 @@
 fun main(){
+    var wordOne="Modern "
+    var wordTwo="Consice "
+    var wordThree="Pragmatic "
+    var wordFour="Safe"
     val kata="""
-        Kotlin is Modern
-        Kotlin is Concise
-        Kotlin is Pragmatic
-        Kotlin is Safe
+        Kotlin is $wordOne
+        Kotlin is $wordTwo
+        Kotlin is $wordThree
+        Kotlin is $wordFour
     """.trimIndent()
     print(kata)
 }
